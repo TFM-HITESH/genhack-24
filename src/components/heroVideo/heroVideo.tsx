acd4c6f9e8293919ed1f482b09ce66366083a84c
TFM-HITESH <hiteshshivkumar@gmail.com>
2024-09-18 01:29:24 +0530

    Quickfix

--- src/components/heroVideo/heroVideo.tsx
+++ src/components/heroVideo/heroVideo.tsx
@@ -39,7 +39,6 @@ export default function HeroVideo() {
                 >
                     <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                     <span className="inline-flex  h-full w-full cursor-pointer items-center justify-center rounded-2xl bg-slate-950 p-5 font-medium text-white backdrop-blur-3xl text-4xl">
-                        
                         <RotateText />
                     </span>
                 </Link> */}
