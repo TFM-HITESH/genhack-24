b534508312f514e5c3248a52833664abebbbb817
parthgoyal974 <parth.goyal@gmail.com>
2024-09-18 02:48:50 +0530

    feat(NavbarComponent): Added ISA logo

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import isaLogo from '@/Assets/ISALogo.png';
 export default function Navbar() {
     return (
       <div className="w-full bg-fuchsia-900 shadow-md sticky top-11 z-50 rounded-full mx-6 backdrop-filter backdrop-blur-lg bg-opacity-60 firefox:bg-opacity-60">
         <nav id="navbar">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-11">
-            <div className="flex items-center justify-between h-16">
+            <div className="flex items-center justify-between h-18">
               <div className="flex-1 flex lg:justify-center lg:space-x-14 xl:space-24">
-                <a href="#" className="text-xl font-bold text-white px-3 py-4">Logo</a>
-                <div className="hidden lg:ml-6 lg:flex lg:space-x-14 xl-space-24">
+                <a href="#" className="text-xl font-bold text-white px-3 py-3">
+                <Image src={isaLogo} className="w-11 h-8" alt="ISA Logo"/>
+                </a>
+                <div className="justify-between hidden lg:ml-6 lg:flex lg:space-x-14 xl-space-24">
                 <Link href="#prizepool" className="text-xl font-bold text-white hover:text-gray-700 px-3 py-4">
                   Prizes
                 </Link>
