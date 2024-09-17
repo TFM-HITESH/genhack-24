1ee7033e27d080eaa2dc0bde4bc62176098d2f2e
parthgoyal974 <parth.goyal@gmail.com>
2024-09-18 03:15:11 +0530

    feat(prizePoolComponent): Added prizepool component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+const nextConfig = {
+    images: {
+      domains: ['cdn.prod.website-files.com'],
+    },
+  };
+  
+  export default nextConfig;
