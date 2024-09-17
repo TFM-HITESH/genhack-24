062c00522eb608c127d3b357b5e65532adfe13fc
Yash Hirani <yashhirani.0055@gmail.com>
2024-09-17 17:57:48 +0530

    feat:added faqs component

--- src/components/faqs/faqs.tsx
+++ src/components/faqs/faqs.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import { Accordion } from './ui/accordion';
+import { AccordionContent } from './ui/accordion';
+import { AccordionItem } from './ui/accordion';
+import { AccordionTrigger } from './ui/accordion';
 
 export default function Faqs() {
     return (
-        <div className="w-full rounded-3xl flex flex-col items-center justify-center bg-blue-400 border-white border-2">
-            faqs : YASH HIRANI
-            <p>Refer to - https://ui.shadcn.com/docs/components/accordion</p>
-            <p>pls learn shadcnui b4 attempting this. its ez</p>
-            <p>
-                Already installed Shadcnui, just install the particular
-                accordion
-            </p>
+        <div className="w-full rounded-3xl flex flex-col items-center justify-center bg-black">
+            <span className="text-4xl font-bold px-6 py-3 text-stone-50 rounded-md bg-gradient-to-r from-purple-700 to-violet-500">
+                Frequently Asked Questions
+            </span>
+            <br></br>
+            <Accordion type="single" collapsible className="w-full ">
+                <AccordionItem value="item-1">
+                    <AccordionTrigger>
+                        Where to find water in rajasthan?
+                    </AccordionTrigger>
+                    <AccordionContent>lol</AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-2">
+                    <AccordionTrigger>What will you learn?</AccordionTrigger>
+                    <AccordionContent>we don't know.</AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-3">
+                    <AccordionTrigger>
+                        Why should you register?
+                    </AccordionTrigger>
+                    <AccordionContent>plj</AccordionContent>
+                </AccordionItem>
+            </Accordion>
         </div>
     );
 }
