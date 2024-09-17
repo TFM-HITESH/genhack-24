53058db6f8ff224127fbaf5d8488920aa6f2e184
Rishikesh Iyer <112635055+Rishikesh-Iyer@users.noreply.github.com>
2024-09-18 01:03:58 +0530

    feat:added footer component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,37 +1,42 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import gravitasLogo from '@/Assets/gravitasLogo.svg';
+import isaLogo from '@/Assets/ISALogo.png';
 
 export default function Footer() {
     return (
         <div className="w-full rounded-3xl bg-black">
             <br></br>
-            <div className='flex justify-evenly' >
+            <br></br>
+            <div className='flex flex-wrap justify-evenly' >
                 <div>
-                    <a href="https://linktr.ee/ISA_VIT" className="flex mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="GenHack Logo" />
-                        <span className="s text-2xl font-semibold whitespace-nowrap dark:text-white">ISA-VIT</span>
-                    </a>
+                    <Link href="https://linktr.ee/ISA_VIT" className="flex mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                        <Image src={isaLogo} className="w-32 h-22" alt="ISA Logo" />
+                    </Link>
                 </div>
                 <div>
-                    <a href="https://gravitas.vit.ac.in/" className="flex mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src='https://flowbite.com/docs/images/logo.svg' className="h-8" alt="GraVITas Logo"/>
-                        <span className="text-2xl font-semibold whitespace-nowrap dark:text-white">GraVITas</span>
-                    </a>
+                    <Link href="https://gravitas.vit.ac.in/" className="flex mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                        <Image src={gravitasLogo} className="w-96 h-24" alt="GraVITas Logo"/>
+                    </Link>
                 </div>
             </div>
             <br></br>
+            <br></br>
             <div className="items-center">
-                <ul className="flex flex-wrap justify-center mb-6 text-sm font-medium text-white sm:mb-0 dark:text-gray-400">
+                <ul className="flex flex-wrap justify-evenly mb-6 text-base font-medium text-white sm:mb-0">
                     <li>
-                        <a href="https://www.linkedin.com/company/international-society-of-automation-isa-vit/" className="hover:underline me-4 md:me-6">LinkedIn</a>
+                        <Link href="https://www.linkedin.com/company/international-society-of-automation-isa-vit/" className="tracking-[1.0em] hover:text-violet-400 me-4 md:me-6">LINKEDIN</Link>
                     </li>
                     <li>
-                        <a href="https://www.instagram.com/isa_vit_/?hl=en" className="hover:underline me-4 md:me-6">Instagram</a>
+                        <Link href="https://www.instagram.com/isa_vit_/?hl=en" className="tracking-[1.0em] hover:text-violet-400 me-4 md:me-6">INSTAGRAM</Link>
                     </li>
                     <li>
-                        <a href="https://www.facebook.com/isavitchapter" className="hover:underline">Facebook</a>
+                        <Link href="https://www.facebook.com/isavitchapter" className="tracking-[1.0em] hover:text-violet-400">FACEBOOK</Link>
                     </li>
                 </ul>
             </div>
+            <br></br>
             <br></br>
         </div>
     );
