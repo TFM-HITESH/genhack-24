702d5a90f09e7d52531ad391ee35cf23e56b7b3b
Rishikesh Iyer <112635055+Rishikesh-Iyer@users.noreply.github.com>
2024-09-17 19:55:34 +0530

    feat:added footer component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,17 +4,11 @@ export default function Footer() {
     return (
         <div className="w-full rounded-3xl bg-black">
             <br></br>
-            <div className='flex justify-around' >
+            <div className='flex justify-evenly' >
                 <div>
                     <a href="https://linktr.ee/ISA_VIT" className="flex mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                         <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="GenHack Logo" />
                         <span className="s text-2xl font-semibold whitespace-nowrap dark:text-white">ISA-VIT</span>
-                    </a>
-                </div>
-                <div>
-                    <a href="https://gravitas.vit.ac.in/events/6b8842bf-3661-45de-9428-47f5996341b4" className="flex mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="ISA Logo" />
-                        <span className=" text-2xl font-semibold whitespace-nowrap dark:text-white">GenHack24</span>
                     </a>
                 </div>
                 <div>
@@ -27,9 +21,6 @@ export default function Footer() {
             <br></br>
             <div className="items-center">
                 <ul className="flex flex-wrap justify-center mb-6 text-sm font-medium text-white sm:mb-0 dark:text-gray-400">
-                    <li>
-                        <a href="#" className="hover:underline me-4 md:me-6">Contact Us</a>
-                    </li>
                     <li>
                         <a href="https://www.linkedin.com/company/international-society-of-automation-isa-vit/" className="hover:underline me-4 md:me-6">LinkedIn</a>
                     </li>
