838c1388431ae1b50c75c468d27462737543afa2
AnishaDhoot <anishadhoot26@gmail.com>
2024-09-18 13:21:45 +0530

    feat:changed the code for better reading

--- src/components/judgingCriteria/judgeCard.tsx
+++ src/components/judgingCriteria/judgeCard.tsx
@@ -1,33 +1,3 @@
-// 'use client';
-// import Image from 'next/image';
-
-// interface JudgingCardProps {
-//     svg: string;
-//     title: string;
-//     content: string;
-// }
-
-// const JudgingCard: React.FC<JudgingCardProps> = ({
-//     svg,
-//     title,
-//     content,
-// }) => {
-//     return (
-//         <div
-//             className="w-[90%] border-black bg-[#F5F5F5] p-4 md:p-8 rounded-2xl md:rounded-[3rem] flex flex-col flex-wrap items-center  "
-//         >
-//             <Image src={svg} width={80} alt="template" className="mb-8" />
-//             <h2 className="text-black text-center text-4xl mb-12 font-extralight">
-//                 {title}
-//             </h2>
-//             <h4 className="text-black text-center text-lg overflow-hidden">
-//                 {content}
-//             </h4>
-//         </div>
-//     );
-// };
-
-// export default JudgingCard;
 
 'use client';
 import Image from 'next/image';
