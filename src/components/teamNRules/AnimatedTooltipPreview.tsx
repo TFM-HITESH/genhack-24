382605c7498e4b3bcd6ba8a30cd701819ed8d9ee
parthgoyal974 <parth.goyal@gmail.com>
2024-09-20 03:15:05 +0530

    feat(teamNRules): fixed type error

--- src/components/teamNRules/AnimatedTooltipPreview.tsx
+++ src/components/teamNRules/AnimatedTooltipPreview.tsx
@@ -1,30 +1,27 @@
 "use client";
 import React from "react";
 import { AnimatedTooltip } from "../ui/animated-tooltip";
-import image1 from './assets/image1.avif';
-import image2 from './assets/image2.avif';
-import image3 from './assets/image3.avif';
 const people = [
   {
     id: 1,
     name: "John Doe",
     designation: "Software Engineer",
     image:
-      image1,
+      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
   },
   {
     id: 2,
     name: "Robert Johnson",
     designation: "Product Manager",
     image:
-      image2,
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
   },
   {
     id: 3,
     name: "Jane Smith",
     designation: "Data Scientist",
     image:
-      image3,
+      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
   },
 ];
 
