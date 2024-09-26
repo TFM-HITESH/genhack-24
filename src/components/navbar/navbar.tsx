d55384992aacde9d09f8d1860caeb86b6b15991b
parthgoyal974 <parth.goyal@gmail.com>
2024-09-27 02:50:42 +0530

    Fix: removed additional navbar space

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import isaLogo from '@/Assets/ISALogo.png';
+
 export default function Navbar() {
     return (
-        <div className="bg-violet-300/30 shadow-md sticky top-11 z-50 rounded-full mx-6 backdrop-filter backdrop-blur-2xl bg-opacity-60 firefox:bg-opacity-60">
-            <nav id="navbar">
+        <div className="fixed top-0 left-0 right-0 z-50 m-11">
+            <nav id="navbar" className="bg-violet-300/30 shadow-md rounded-full mx-6 backdrop-filter backdrop-blur-2xl bg-opacity-60 firefox:bg-opacity-60">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-11">
                     <div className="flex items-center justify-between h-18">
                         <div className="flex-1 flex lg:justify-center lg:space-x-14 xl:space-24">
