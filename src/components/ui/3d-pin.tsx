ddf3033961e947e91b2486b1f83c3ab886577b6e
TFM-HITESH <hiteshshivkumar@gmail.com>
2024-09-27 08:07:15 +0530

    blank fixed

--- src/components/ui/3d-pin.tsx
+++ src/components/ui/3d-pin.tsx
@@ -37,6 +37,7 @@ export const PinContainer = ({
             onMouseEnter={onMouseEnter}
             onMouseLeave={onMouseLeave}
             href={href || '/'}
+            target="_blank"
         >
             <div
                 style={{
